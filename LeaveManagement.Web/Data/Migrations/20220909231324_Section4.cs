31a6e7a0617511eeebfbbcf76551ca000d6b9386
gedwards1988 <108200463+gedwards1988@users.noreply.github.com>
2022-09-10 00:15:26 +0100

    End of Section 4 Commit

---
+++ LeaveManagement.Web/Data/Migrations/20220909231324_Section4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    public partial class Section4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateJoined",
+                table: "LeaveAllocations");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "LeaveAllocations");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "LeaveAllocations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "LeaveAllocations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "LeaveAllocations");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "LeaveAllocations");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateJoined",
+                table: "LeaveAllocations",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "LeaveAllocations",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
