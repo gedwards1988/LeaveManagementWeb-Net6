d5d7a806a43acdc80a81c6d9f40c353fdfb7aa81
gedwards1988 <108200463+gedwards1988@users.noreply.github.com>
2022-09-10 00:12:07 +0100

    End of Section 4

--- LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -3,11 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        // Create a table based on the LeaveType class and call it LeaveTypes
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        // Create a table based on the LeaveAllocation class and call it LeaveAllocations
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
     }
 }
---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    // Cannot do it by itself - every table we create will have these base columns
+    public abstract class BaseEntity
+    {
+        // 1. Always have a Primary Key - Auto Increments
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
