e69d29933003cb0ad06d4500225f5b2de2c8e8d6
gedwards1988 <108200463+gedwards1988@users.noreply.github.com>
2022-09-16 16:21:01 +0100

    Update

---
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Web.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllSync();
+        Task<int> GetCountAsync();
+
+
+    }
+}
--- LeaveManagement.Web/Data/LeaveAllocation.cs
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -5,6 +5,7 @@ namespace LeaveManagement.Web.Data
     public class LeaveAllocation : BaseEntity
     { 
         public int NumberOfDays { get; set; }
+
         [ForeignKey("LeaveTypeId")]
         public LeaveType LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
